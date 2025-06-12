375a53d5edc240ce10e4ef6cd7372e123ccf0aa9
Petr Novák <petr.novak@performax.cz>
2025-06-12 17:33:12 +0200

    pridani do configu base

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/rpg-cv/',
   server: {
     host: "::",
     port: 8080,
